565df79f3fda780a7a02bae15b13c7b7739123d8
Hamna <97177481+hamna4503@users.noreply.github.com>
2022-04-26 12:47:59 +0500

    Add files via upload

---
+++ 2_1LAB2.cpp
@@ -0,0 +1,13 @@
+// This program will write the name, address and telephone 
+// number of the programmer.
+// PLACE YOUR NAME HERE
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hamna Aamir"<<endl;
+    cout<<"F-123 Block F North Nazimabad Karachi"<<endl;
+    cout<<"Karachi , Sindh, 74600"<<endl;
+    cout<<"03320354435"<<endl;
+return 0;
+}
